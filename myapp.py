7725f74b9cf527f6ce017f3f4e0b14a320e7d14c
lukeconibear <earlacoa@leeds.ac.uk>
2020-01-28 20:05:03 +0000

    updating

--- myapp.py
+++ myapp.py
@@ -1,9 +1,24 @@
+import sys, os
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pages=pages)
+
+@app.route("/<path:path>/")
+def page(path):
+#    return pages.get_or_404(path).html
+    page = pages.get_or_404(path)
+    return render_template("page.html", page=page)
 
 if __name__ == '__main__':
     app.run(debug=True)
