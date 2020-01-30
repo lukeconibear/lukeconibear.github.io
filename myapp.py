fd15171511b1ff716fd24670312b8ff6accaa40d
lukeconibear <earlacoa@leeds.ac.uk>
2020-01-30 15:26:22 +0000

    test

--- myapp.py
+++ myapp.py
@@ -21,7 +21,7 @@ def page(path):
 
 @app.route("/software/")
 def software():
-    return render_template('software.html')
+    return render_template('software.html', pages=pages)
 
 @app.route("/airpollution/")
 def airpollution():
